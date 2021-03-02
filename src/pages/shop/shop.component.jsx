7ab895db691adef72823ac38f596c2062b7c3e7b
Aaron Graham <aarongraham518@gmail.com>
2021-03-01 20:19:48 -0500

    updating our app with a shoppage, collection preview and item cp

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// import SHOP_DATA from './shop.data.js';
+import SHOP_DATA from './shopdataonline.js';
+
+
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    
+    render(){
+        console.log(SHOP_DATA);
+        const {collections} = this.state;
+        return(
+            <div className="shop-page">
+                {
+                    collections.map(({id,...otherCollectionProps}) => (
+                        <CollectionPreview 
+                            key={id}
+                            {...otherCollectionProps}
+                        />
+                    ))
+                }
+            </div>)
+    }
+}
+
+export default ShopPage;
