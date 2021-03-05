e3be615370cb74cf3d00af240d0973a494db930e
Aaron Graham <aarongraham518@gmail.com>
2021-03-05 14:33:04 -0500

    added google firebase (auth) to the project

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+//firebase keyword gives us access to base
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAoz4W178YmjspvlC70mMPvy__RShgke34",
+    authDomain: "crwndb-947dd.firebaseapp.com",
+    projectId: "crwndb-947dd",
+    storageBucket: "crwndb-947dd.appspot.com",
+    messagingSenderId: "718890844110",
+    appId: "1:718890844110:web:f0c18b8fa0b96cfa17f59f"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  //access to new google auth class from auth library
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account'}); //trigger google popup for auth/signin
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
